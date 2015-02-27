06087468f0c19e915ca38b818f3b1a33c39d0d4b
David Gomes <davidrafagomes@gmail.com>
2015-02-27 12:05:31 +0000

    Makes Simulation not static.

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,8 @@ import src.simulation.*;
 public class Main {
 
 	private static final int N_ITERATIONS = 5000;
-	private static Simulation simulation;
 
 	public static void main(String[] args) {
-		simulation = new Simulation(N_ITERATIONS);
+		Simulation simulation = new Simulation(N_ITERATIONS);
 	}
 }
